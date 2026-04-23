61ed00a27404f44f461cbc17dca4e83b3ea79c83
Alexander Usov <usov3299@gmail.com>
2026-04-23 15:17:45 +0300

    сделал допы по 1, 4, 5

--- src/javaapplication2/RecIntegral.java
+++ src/javaapplication2/RecIntegral.java
@@ -4,9 +4,13 @@
  */
 package javaapplication2;
 
-import java.io.Serializable;
+import java.io.Externalizable;
+import java.io.IOException;
+import java.io.ObjectInput;
+import java.io.ObjectOutput;
 
-public class RecIntegral implements Serializable {
+
+public class RecIntegral implements Externalizable {
     private static final long serialVersionUID = 1L;
     
     static int counter = 0;
@@ -15,6 +19,10 @@ public class RecIntegral implements Serializable {
     double b;
     double h;
     double result;
+    
+    public RecIntegral() {
+        
+    }
 
     public RecIntegral(double a, double b, double h, double result) throws InvalidException{
         this.a = a;
@@ -27,6 +35,30 @@ public class RecIntegral implements Serializable {
     private boolean isValid(double x) {
         return x >= 0.000001 && x <= 1_000_000;
     }
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeInt(id);
+        out.writeDouble(a);
+        out.writeDouble(b);
+        out.writeDouble(h);
+        out.writeDouble(result);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        id = in.readInt();
+        a = in.readDouble();
+        b = in.readDouble();
+        h = in.readDouble();
+        result = in.readDouble();
+        // Опционально: проверка корректности загруженных данных
+        if (!isValid(a) || !isValid(b) || !isValid(h)) {
+            throw new IOException("Некорректные значения a, b, h при загрузке");
+        }
+    }
+    
+    
 }
 
     
